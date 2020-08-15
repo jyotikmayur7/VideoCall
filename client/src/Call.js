ccc280759d07b333801dfbc89c1cad1ba5885919
orangekun9 <heyjyotik@gmail.com>
2020-08-15 13:28:47 +0100

    Added different state variables and made the required socket calls

--- client/src/Call.js
+++ client/src/Call.js
@@ -4,6 +4,7 @@ import Peer from 'simple-peer';
 
 function Call() {
     const [yourID, setYourID] = useState("");
+    const [username, setUsername] = useState("User");
     const [users, setUsers] = useState({});
     const [stream, setStream] = useState();
     const [receivingCall, setReceivingCall] = useState(false);
@@ -14,6 +15,33 @@ function Call() {
     const userVideo = useRef();
     const partnerVideo = useRef();
     const socket = useRef();
+
+    useEffect(() => {
+        socket.current = io.connect("");
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
+            setStream(stream);
+            if (userVideo.current) {
+                userVideo.current.srcObject = stream;
+            }
+        })
+
+        socket.current.on("yourID", (id) => {
+            setYourID(id);
+        })
+
+        socket.current.emit("username", username + Math.floor((Math.random() * 100) + 1));
+
+        socket.current.on("usersData", (users) => {
+            setUsers(users);
+        })
+
+        socket.current.on("hey", (data) => {
+            setReceivingCall(true);
+            setCaller(data.from);
+            setCallerSignal(data.signal);
+        })
+
+    }, [])
 }
 
 export default Call;
