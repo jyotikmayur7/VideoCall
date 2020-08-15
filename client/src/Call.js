f6f62465f0b77918b29dcd28d1727074a5a2154a
orangekun9 <heyjyotik@gmail.com>
2020-08-15 12:49:23 +0100

    Added basic structure in client side

---
+++ client/src/Call.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState, useRef } from 'react';
+import io from 'socket.io-client';
+import Peer from 'simple-peer';
+
+function Call() {
+    const [yourID, setYourID] = useState("");
+    const [users, setUsers] = useState({});
+    const [stream, setStream] = useState();
+    const [receivingCall, setReceivingCall] = useState(false);
+    const [caller, setCaller] = useState("");
+    const [callerSignal, setCallerSignal] = useState();
+    const [callAccepted, setCallAccepted] = useState(false);
+
+    const userVideo = useRef();
+    const partnerVideo = useRef();
+    const socket = useRef(); s
+}
+
+export default Call;
